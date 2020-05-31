4b65b44102e88c6a33cec24d8e0187f26e83398b
Amir Khakshour <amir.khakshour@evernest.com>
2020-05-31 10:49:07 +0200

    added cocktail models

--- cocktailor/cocktail/models.py
+++ cocktailor/cocktail/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+from django.template.defaultfilters import date as date_filter
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    """Describes an ingredient for cocktail."""
+    name = models.CharField(max_length=255)
+    alcoholic = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+
+class AvailableIngredient(models.Model):
+    """Describes an ingredient for cocktail. This ingredient contains attributes like
+    expiration date and type of drink, weather it's alcoholic or not."""
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    expire_date = models.DateTimeField(_('expire date'), db_index=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        date_format = settings.DATETIME_FORMAT
+        return 'AvailableIngredient %s  expire_date=%s ' % \
+               (self.ingredient, date_filter(self.expire_date, date_format))
+
+
+class Cocktail(models.Model):
+    """Basic object representing a Cocktail which includes different ingredients."""
+    name = models.CharField(max_length=255)
+    ingredients = models.ManyToManyField(Ingredient)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
